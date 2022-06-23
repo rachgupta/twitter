d4c467d06e6d7fddc35dcac1a8fc740264908340
Rachna Gupta <rachnagupta@fb.com>
2022-06-22 17:33:56 -0700

    added ComposeViewController and implemented close button

---
+++ fbu_twitter_starter/twitter/View Controllers/ComposeViewController.h
@@ -0,0 +1,17 @@
+//
+//  ComposeViewController.h
+//  twitter
+//
+//  Created by Rachna Gupta on 6/22/22.
+//  Copyright © 2022 Emerson Malca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ComposeViewController : UICollectionViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
